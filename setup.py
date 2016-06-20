8e47f7cee724111ab7fda354a8dfc2f48d658a11
tyler <phillipst55@gmail.com>
2016-06-20 17:28:54 -0400

    Added description from README

--- setup.py
+++ setup.py
@@ -3,10 +3,13 @@
 import os
 from setuptools import setup
 
+with open('README.md') as f:
+    readme = f.read()
 
 setup(
     name= 'my_app',     #replace later?
     version= '0.0.1',
+    description=readme,
     url= 'https://github.com/Mester/demo-day-vikings',
     packages= ['my_app']
 )

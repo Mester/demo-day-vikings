5774e45c1e768649a2919a1a823885e45500b464
jasonmeeks <jasonmeeks42@gmail.com>
2016-06-20 18:05:35 -0500

    Added api_access.py
    Basic code to access reddit API. Need to copy the access information I
    posted in slack at the top of code for this to work

---
+++ my_app/api_access.py
@@ -0,0 +1,28 @@
+import requests
+from pprint import pprint as pp
+
+client_id = "j0kVJeOyMoGKTA"
+client_secret = "AUTapOwecWmrmOV4y65iITxUVuk"
+
+song_posts = []
+
+client_auth = requests.auth.HTTPBasicAuth(client_id, client_secret)
+post_data = {"grant_type": "client_credentials"}
+reddit_headers = {"User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+r = requests.post("https://www.reddit.com/api/v1/access_token", auth=client_auth, data=post_data, headers=reddit_headers)
+print "### ACCESS TOKEN REQUEST ###"
+print r.status_code
+
+if r.ok:
+    pp(r.json())
+    token = r.json()['access_token']
+
+    headers = {"Authorization": "bearer " + token, "User-Agent": "ChangeMeClient/0.1 by YourUsername"}
+    r = requests.get("https://oauth.reddit.com/r/ListenToThis/hot", headers=headers)
+    print r.status_code
+    print "### REDDIT API DATA REQUEST ###"
+    if r.ok:
+        for post in r.json()['data']['children']:
+            song_posts.append(post['data']['title'])
+
+pp(song_posts)

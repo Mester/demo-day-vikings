e233cc0fa58b0937595978863663e8acf1e458df
tyler <phillipst55@gmail.com>
2016-06-20 17:48:00 -0400

    More changes to setup

--- setup.py
+++ setup.py
@@ -6,10 +6,18 @@ from setuptools import setup
 with open('README.md') as f:
     readme = f.read()
 
+
+with open('LICENSE') as f:
+    license = f.read()
+
+
 setup(
     name= 'my_app',     #replace later?
     version= '0.0.1',
-    description=readme,
+    description='Generate a list of 10 trending songs from r/listentothis',
+    long_description=readme,
+    authors='Ben Dauer, Adrian Hintermaier, Jason Meeks, Tyler Phillips, Anubhav Yadav',
     url= 'https://github.com/Mester/demo-day-vikings',
+    license=license,
     packages= ['my_app']
 )

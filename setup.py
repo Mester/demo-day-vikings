e5a1f1b4d8724718733c6a39797904294b92a6c9
tyler <phillipst55@gmail.com>
2016-06-20 17:23:43 -0400

    Basic setup

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
+# -*- coding: utf-8 -*-
 
+import os
+from setuptools import setup
+
+
+setup(
+    name= 'my_app',     #replace later?
+    version= '0.0.1',
+    url= 'https://github.com/Mester/demo-day-vikings',
+    packages= ['my_app']
+)

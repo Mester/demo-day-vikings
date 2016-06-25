801ad564642eb6dddc09b00daecb82ccf43b8d55
bdauer <dauer.benjamin@gmail.com>
2016-06-25 09:04:33 -0400

    Render index for testing

--- my_app/main.py
+++ my_app/main.py
@@ -1,12 +1,10 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
-
-
+    return render_template("index.html")

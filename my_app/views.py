a58e0de8002145e7875b2d075cca4126ecba523a
Anubhav Yadav <anubhav1691@gmail.com>
2016-06-30 02:30:00 +0530

    Fixes #39
    * Markdown dependency added in requirements.txt
    * Made the change in about.html to change the link of about
    * Added the code to render the about page in the views.py
    * Added some more code here and there, it's 3 AM can't remember

--- my_app/views.py
+++ my_app/views.py
@@ -1,7 +1,7 @@
 import os
-
+import markdown
 from flask import Flask, render_template
-
+from flask import Markup
 from my_app import app
 
 
@@ -17,3 +17,10 @@ def show_results():
     #next_listing()
     #other data handling
     return render_template('results.html')
+
+@app.route('/about')
+def show_about():
+    with open('README.md') as f:
+        readme = Markup(markdown.markdown(f.read()))
+        print readme
+    return render_template('about.html', readme=readme)

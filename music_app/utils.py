609845cb60f05056f7e0362a5f54e25d1df0a005
tyler <phillipst55@gmail.com>
2016-07-02 14:11:23 -0400

    fixed again for py2

--- music_app/utils.py
+++ music_app/utils.py
@@ -4,6 +4,10 @@ try:
 except ImportError:
     import urlparse
 import collections
+try:
+    import collections.abc as collections
+except ImportError:
+    import collections
 
 
 def parse_listing(data):
@@ -23,7 +27,7 @@ def flatten(d, parent_key='', sep='_'):
     items = []
     for k, v in d.items():
         new_key = parent_key + sep + k if parent_key else k
-        if isinstance(v, collections.abc.MutableMapping):
+        if isinstance(v, collections.MutableMapping):
             items.extend(flatten(v, new_key, sep=sep).items())
         else:
             items.append((new_key, v))

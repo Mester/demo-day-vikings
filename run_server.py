b570f8f5d7541d70eec9d1d43f18984df3b0c950
Anubhav Yadav <anubhav1691@gmail.com>
2016-07-02 23:37:35 +0530

    Removed unecessary whitespaces

--- run_server.py
+++ run_server.py
@@ -8,8 +8,6 @@ if __name__ == '__main__':
     app.debug = True
     app.secret_key = binascii.hexlify(os.urandom(24))
     app.config['DATABASE'] = (0, settings.DATABASE_NAME)
-
     host = os.environ.get('IP', '0.0.0.0')
     port = int(os.environ.get('PORT', 8080))
     app.run(host=host, port=port)
-
